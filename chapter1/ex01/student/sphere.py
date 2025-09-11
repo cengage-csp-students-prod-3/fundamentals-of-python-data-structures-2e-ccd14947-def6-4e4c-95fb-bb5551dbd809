adeddd1c174ded7bde356f7cf77c60bfb83b0486
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-09-11 16:40:19 +0000

    Testing Thu Sep 11 2025 16:40:19 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex01/student/sphere.py
+++ chapter1/ex01/student/sphere.py
@@ -3,6 +3,6 @@ import math
 
 radius = float(input("Enter the sphere's radius: "))
 print("Diameter: ", radius * 2)
-print("Circumference: ", radius * 2 * 3.14)
-print("Surface area: ", radius ** 2 * 4 * 3.14)
-print("Volume: ", 4/3 * 3.14 * radius ** 3)
+print("Circumference: ", radius * 2 * 3.1459)
+print("Surface area: ", radius ** 2 * 4 * 3.1459)
+print("Volume: ", 4/3 * 3.1459 * radius ** 3) 
