a1a944084b43885b79db57ae57744d009814ba5d
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-09-12 21:10:26 +0000

    Testing Fri Sep 12 2025 21:10:26 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex09/student/numberguess.py
+++ chapter1/ex09/student/numberguess.py
@@ -1,25 +1,41 @@
 import math
+import random
 
 def main():
-    low =int(input("Enter the smaller number:"))
-    
-    high =int(input("Enter the larger number:"))
-
-    yourNumber = round(math.log(high - low + 1,2))
+    low =int(input("Enter the smaller number:")) #User enters the lower bound as a clue. Only needs to happen once.
+    high =int(input("Enter the larger number:")) #User enters the upper bound as a clue. Only needs to happen once.
+    guessCount = round(math.log(high - low + 1, 2))
     count=0
     
+    yourNumber = random.randint(low, high) #this is the computer's guess
+
     print("Your number is", yourNumber)
 
-    user_response = input("Enter =, <, or >:")
+    while True:
+        count += 1 #Increase the attempt count by one.
 
-    count+=1
+        if count > guessCount: 
+            print ("You're cheating!")
+            break
+        else: 
+            user_response = input("Enter =, <, or >:") #User tells computer if its higher or lower.
 
-    if user_response == ">":
-        print("Your number is", yourNumber)
-    elif user_response == "<":
-            print("Your number is", yourNumber)
-    elif user_response == "=":
-            print("Hooray, I got it in", count, "tries!")
+            if user_response == ">":
+                high = yourNumber - 1 #Whatever computer guessed -1, because we're saying it's less than that number. Update the higher bound.
+                yourNumber = random.randint(low, high) #Should generate a higher number.
+                
+                print("Your number is", yourNumber)
+            elif user_response == "<":
+                low = yourNumber + 1 #Whatever computer guessed +1, because we're saying it's greater than that number. Update the lower bound.
+                yourNumber = random.randint(low, high) #Should generate a lower number
+                
+                print("Your number is", yourNumber)
+            elif user_response == "=":
+                print("Hooray, I got it in", count, "tries!")
+                break
+            else:
+                print("Invalid entry, try again.")
+            
 
-            if __name__ == "__main__":
-                main()
+if __name__ == "__main__":
+    main()
