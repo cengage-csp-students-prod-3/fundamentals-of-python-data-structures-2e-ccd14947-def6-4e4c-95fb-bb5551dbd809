06f0232f9d8fd11abb3f363db8009ae70b397933
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-09-18 02:53:00 +0000

    Testing Thu Sep 18 2025 02:53:00 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex02/student/reverse.py
+++ chapter3/ex02/student/reverse.py
@@ -1,3 +1,4 @@
+
 """
 File: reverse.py
 Project 3.2
@@ -5,25 +6,28 @@ Defines a function to reverse the elements in a list.
 Computational complexity:
 """
 
-def reverse(lyst):
+def reverse(lyst, myList): 
     """Reverses the elements in a list in linear time."""
     # Use indexes to the first and last element
     # and move them toward each other.
-
-def reverse O(n)
-    lyst = list(revrse)
     
+ 
+    for item in lyst:
+        myList.insert(0,lyst[item])
+
 def swap(lyst, x, y):
     """Exchanges the elements at positions x and y."""
     
 def main():
     """Tests with two lists."""
     lyst = list(range(4))
-    reverse(lyst)
-    print(lyst)
+    myList = []
+    reverse(lyst, myList)
+    print(myList)
+    myList.clear()
     lyst = list(range(3))
-    reverse(lyst)
-    print(lyst)
+    reverse(lyst, myList)
+    print(myList)
 
 if __name__ == "__main__":
     main()
