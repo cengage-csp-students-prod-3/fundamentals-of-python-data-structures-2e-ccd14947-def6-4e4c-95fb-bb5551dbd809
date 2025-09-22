0ebebe643650f9611cf148e772839fac8ad684b5
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-09-22 17:09:43 +0000

    Testing Mon Sep 22 2025 17:09:43 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/arrays.py
+++ chapter4/ex04/student/arrays.py
@@ -22,9 +22,10 @@ class Array(object):
 	
 	# Reuse your solution from Programming Exercise 4.3 as your starter file
 
-    def insert()
+    def insert():
+        
 
-    def pop()
+    def pop():
 
     def __init__(self, capacity, fillValue = None):
         """Capacity is the static size of the array.
