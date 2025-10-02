1ac98ebf387fe70588f0d19297274621eb7382c5
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-10-02 17:27:53 +0000

    Reset Exercise Thu Oct 02 2025 17:27:53 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex03/student/5.3/arraybag.py
+++ chapter5/ex03/student/5.3/arraybag.py
@@ -1,20 +1,22 @@
 """
-Project 5.5
-File: linkedbag.py
+Project 5.3
+File: arraybag.py
 Author: Ken Lambert
 """
+from arrays import Array
 
-from node import Node
+class ArrayBag(object):
+    """An array-based bag implementation."""
 
-class LinkedBag(object):
-    """A link-based bag implementation."""
+    # Class variable
+    DEFAULT_CAPACITY = 10
 
     # Constructor
     def __init__(self, sourceCollection = None):
         """Sets the initial state of self, which includes the
         contents of sourceCollection, if it's present."""
-        self.items = None
-        self.size = 0
+        self._items = Array(ArrayBag.DEFAULT_CAPACITY)
+        self._size = 0
         if sourceCollection:
             for item in sourceCollection:
                 self.add(item)
@@ -25,8 +27,8 @@ class LinkedBag(object):
         return len(self) == 0
     
     def __len__(self):
-        """-Returns the number of items in self."""
-        return self.size
+        """Returns the number of items in self."""
+        return self._size
 
     def __str__(self):
         """Returns the string representation of self."""
@@ -34,23 +36,18 @@ class LinkedBag(object):
 
     def __iter__(self):
         """Supports iteration over a view of self."""
-        cursor = self.items
-        while not cursor is None:
-            yield cursor.data
-            cursor = cursor.next
+        cursor = 0
+        while cursor < len(self):
+            yield self._items[cursor]
+            cursor += 1
 
     def __add__(self, other):
         """Returns a new bag containing the contents
         of self and other."""
-        result = LinkedBag(self)
+        result = ArrayBag(self)
         for item in other:
             result.add(item)
         return result
-
-    def clone(self):
-        """Returns a copy of self."""
-        
-        
 
     def __eq__(self, other):
         """Returns True if self equals other,
@@ -67,13 +64,19 @@ class LinkedBag(object):
     # Mutator methods
     def clear(self):
         """Makes self become empty."""
-        self.size = 0
-        self.items = None
+        self._size = 0
+        self._items = Array(ArrayBag.DEFAULT_CAPACITY)
 
     def add(self, item):
         """Adds item to self."""
-        self.items = Node(item, self.items)
-        self.size += 1
+        # Check array memory here and increase it if necessary
+        if len(self) == len(self._items):
+            temp = Array(2 * len(self))
+            for i in range(len(self)):
+                temp[i] = self[i]
+            self._items = temp
+        self._items[len(self)] = item
+        self._size += 1
 
     def remove(self, item):
         """Precondition: item is in self.
@@ -82,23 +85,23 @@ class LinkedBag(object):
         # Check precondition and raise if necessary
         if not item in self:
             raise KeyError(str(item) + " not in bag")
-        # Search for the node containing the target item
-        # probe will point to the target node, and trailer
-        # will point to the one before it, if it exists
-        probe = self.items
-        trailer = None
+        # Search for the index of the target item
+        targetIndex = 0
         for targetItem in self:
             if targetItem == item:
                 break
-            trailer = probe
-            probe = probe.next
-        # Unhook the node to be deleted, either the first one or one
-        # thereafter
-        if probe == self.items:
-            self.items = self.items.next
-        else:
-            trailer.next = probe.next
+            targetIndex += 1
+        # Shift items to the left of target up by one position
+        for i in range(targetIndex, len(self) - 1):
+            self._items[i] = self._items[i + 1]
         # Decrement logical size
-        self.size -= 1
+        self._size -= 1
+        # Check array memory here and decrease it if necessary
+        if len(self) < len(self._items) // 3 and \
+           2 * len(self) >= ArrayBag.DEFAULT_CAPACITY:
+            temp = Array(len(self._items) // 2)
+            for i in range(len(self)):
+                temp[i] = self[i]
+            self._items = temp
         
         
