5a8728f720bb65ee2aefb266159df9779a2b6e71
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-10-03 23:44:37 +0000

    Testing Fri Oct 03 2025 23:44:37 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex04/student/testbag.py
+++ chapter5/ex04/student/testbag.py
@@ -5,6 +5,7 @@ A tester program for bag implementations.
 """
 
 from arraybag import ArrayBag
+from linkedbag import LinkedBag
 
 def test(bagType):
     """Expects a bag type as an argument and runs some tests
@@ -19,20 +20,18 @@ def test(bagType):
         b.add(i)
     print("Expect the bag's string:", b)
 
-def testResize():
+def testClone(bagType):
     """Tests the resizing of an array-based bag,
     when space is wasted."""
-    bag = ArrayBag(range(100))
-    print("Added 100 items, length of bag =", len(bag))
-    print("Expect 160 as length of array =", len(bag.items))
-    print(bag)
-    for item in range(76):
-        bag.remove(item)
-    print("Removed 76 items, expect 24 as length of bag:", len(bag))
-    print("Expect 80 as length of array =", len(bag.items))
-    for item in range(76, 100):
-        bag.remove(item)
-    print("Removed remaining items, length of bag =", len(bag))
-    print("Expect 10 as length of array =", len(bag.items))
+    print("Testing", bagType)
+    bag1 = bagType([2,3,4])
+    bag2 = bag1.clone()
+    print("Creating bag1 with 2, 3, and 4.")
+    print("Cloning bag1 to bag2.")
+    print("Expect {2, 3, 4} and {2, 3, 4}.")
+    print(bag1, bag2)
+    print("Expect True for ==:", bag1 == bag2)
+    print("Expect False for is:", bag1 is bag2)
 
-testResize()
+testClone(ArrayBag)
+testClone(LinkedBag)
