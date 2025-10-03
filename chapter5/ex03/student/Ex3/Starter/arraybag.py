b484f21de58a035102c925836125d11ad185dd1b
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-10-03 17:04:07 +0000

    Testing Fri Oct 03 2025 17:04:07 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex03/student/Ex3/Starter/arraybag.py
+++ chapter5/ex03/student/Ex3/Starter/arraybag.py
@@ -89,6 +89,7 @@ class ArrayBag(object):
             self.grow()
         self.items[len(self)]=item
         self.size+=1
+        
         # Check array memory here and increase it if necessary
         
         
