157b93050d6a08c59efe1247c9d1fe2f07fe3f91
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-10-05 17:36:13 +0000

    Testing Sun Oct 05 2025 17:36:13 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex04/student/linkedbag.py
+++ chapter5/ex04/student/linkedbag.py
@@ -45,10 +45,6 @@ class LinkedBag(object):
         for item in other:
             result.add(item)
         return result
-    
-    def clone(self):
-        """Returns a copy of self."""
-        return LinkedBag(self)
 
     def __eq__(self, other):
         """Returns True if self equals other,
