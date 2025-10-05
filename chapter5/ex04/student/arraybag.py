7d33fa97ae3ca85042e152ce81f6003aeb8a4c4d
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-10-05 17:37:45 +0000

    Reset Exercise Sun Oct 05 2025 17:37:45 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex04/student/arraybag.py
+++ chapter5/ex04/student/arraybag.py
@@ -2,12 +2,16 @@
 Project 5.4
 File: arraybag.py
 Author: Ken Lambert
+
+Reuse your solution from Programming Exercise 5.3 as your starter file
 """
 
 from arrays import Array
 
 class ArrayBag(object):
     """An array-based bag implementation."""
+	
+	# Reuse your solution from Programming Exercise 5.3 as your starter file
 
     # Class variable
     DEFAULT_CAPACITY = 10
@@ -76,41 +80,6 @@ class ArrayBag(object):
         self.size = 0
         self.items = Array(ArrayBag.DEFAULT_CAPACITY)
 
-    def add(self, item):
-        """Adds item to self."""
-        # Check array memory here and increase it if necessary
-        if len(self) == len(self.items):
-            temp = Array(2 * len(self))
-            for i in range(len(self)):
-                temp[i] = self.items[i]
-            self.items = temp
-        self.items[len(self)] = item
-        self.size += 1
-
-    def remove(self, item):
-        """Precondition: item is in self.
-        Raises: KeyError if item in not in self.
-        Postcondition: item is removed from self."""
-        # Check precondition and raise if necessary
-        if not item in self:
-            raise KeyError(str(item) + " not in bag")
-        # Search for the index of the target item
-        targetIndex = 0
-        for targetItem in self:
-            if targetItem == item:
-                break
-            targetIndex += 1
-        # Shift items to the left of target up by one position
-        for i in range(targetIndex, len(self) - 1):
-            self.items[i] = self.items[i + 1]
-        # Decrement logical size
-        self.size -= 1
-        # Check array memory here and decrease it if necessary
-        if len(self) <= len(self.items) // 4 and \
-           2 * len(self) >= ArrayBag.DEFAULT_CAPACITY:
-            temp = Array(len(self.items) // 2)
-            for i in range(len(self)):
-                temp[i] = self.items[i]
-            self.items = temp
+        
         
         
