09874bbba9cc71c70c84fd28b1353e963cd08555
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-10-28 19:44:59 +0000

    Testing Tue Oct 28 2025 19:44:59 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03/student/linkedqueue.py
+++ chapter8/ex03/student/linkedqueue.py
@@ -7,6 +7,36 @@ from node import Node
 from abstractcollection import AbstractCollection
 
 class LinkedQueue(AbstractCollection):
+
+    def remove(self, item):
+        """Removes the first occurrence of item from the queue and
+        returns it. Raises KeyError if the item is not found."""
+        if self.isEmpty():
+            raise KeyError(item)
+
+        prev = None
+        cursor = self.front
+        while cursor is not None:
+            if cursor.data == item:
+                # Removing front node
+                if prev is None:
+                    self.front = cursor.next
+                    if self.front is None:
+                        # Queue is now empty
+                        self.rear = None
+                else:
+                    prev.next = cursor.next
+                    if cursor is self.rear:
+                        # Removed the rear node
+                        self.rear = prev
+
+                self.size -= 1
+                return item
+
+            prev = cursor
+            cursor = cursor.next
+
+        raise KeyError(item)
     """A link-based queue implementation."""
 
     # Constructor
@@ -69,38 +99,4 @@ class LinkedQueue(AbstractCollection):
         self.size -= 1
         return oldItem
         
-    def remove(self, item):
-        """Removes the first occurrence of item from the queue and
-        returns it. Raises KeyError if the item is not found."""
-        if self.isEmpty():
-            raise KeyError(item)
-
-        prev = None
-        cursor = self.front
-        while cursor is not None:
-            if cursor.data == item:
-                # Removing front node
-                if prev is None:
-                    self.front = cursor.next
-                    if self.front is None:
-                        # Queue is now empty
-                        self.rear = None
-                else:
-                    prev.next = cursor.next
-                    if cursor is self.rear:
-                        # Removed the rear node
-                        self.rear = prev
-
-                self.size -= 1
-                return item
-
-            prev = cursor
-            cursor = cursor.next
-
-        raise KeyError(item)
-    # End of remove method
-
-        
- 
-
-
+    
