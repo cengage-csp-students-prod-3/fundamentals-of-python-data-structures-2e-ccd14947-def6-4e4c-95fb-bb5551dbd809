5e841d3e4c0af0d4742be4ac3c391c2abebca109
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-11-10 22:56:44 +0000

    Testing Mon Nov 10 2025 22:56:44 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex01/student/linkedbst.py
+++ chapter10/ex01/student/linkedbst.py
@@ -48,11 +48,11 @@ class LinkedBST(AbstractCollection):
 
     def preorder(self):
         """Supports a preorder traversal on a view of self."""
-
+        
 
     def inorder(self):
         """Supports an inorder traversal on a view of self."""
-      
+      lyst = list()
     
     def postorder(self):
         """Supports a postorder traversal on a view of self."""
