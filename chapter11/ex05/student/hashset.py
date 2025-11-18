c3c01759bd697e3d45da137bdbff8cba40fff098
Gabriel6014 <gabriebarrera@my.lonestar.edu>
2025-11-18 02:15:48 +0000

    Testing Tue Nov 18 2025 02:15:48 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex05/student/hashset.py
+++ chapter11/ex05/student/hashset.py
@@ -77,18 +77,18 @@ class HashSet(AbstractSet, AbstractCollection):
         Postcondition: item is removed from self."""
         # Exercise 
         index = hash(item) % self.capacity
-    current = self.array[index]
-    prev = None
+        current = self.array[index]
+        prev = None
 
-    while current is not None:
-        if current.data == item:
-            if prev is None:
-                # Removing head of bucket
-                self.array[index] = current.next
-            else:
-                prev.next = current.next
-            self.size -= 1
-            return
-        prev, current = current, current.next
+        while current is not None:
+            if current.data == item:
+                if prev is None:
+                    # Removing head of bucket
+                    self.array[index] = current.next
+                else:
+                    prev.next = current.next
+                self.size -= 1
+                return
+            prev, current = current, current.next
 
-    raise KeyError(f"{item} not found in HashSet")
+        raise KeyError(f"{item} not found in HashSet")
